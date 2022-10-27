89788b0bb362be3096e58b60cca0d7a4c827e7d4
SIARHEI-SMANTSAR <siarhei.smantsar@gmail.com>
2022-10-28 01:23:17 +0200

    feat: add saveCalls

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from '@app/types';
 export * from '@app/config';
 export * from '@app/save-file';
 export * from '@app/save-functions';
+export * from '@app/save-calls';
--- src/save-functions.ts
+++ src/save-functions.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 import { CACHE_FOLDER_PATH } from '@app/config';
-import { FunctionType, META_FILE_NAME } from '@app/constants';
+import { CALLS_FOLDER_NAME, FunctionType, META_FILE_NAME } from '@app/constants';
 import type { FunctionMeta } from '@app/types';
 import { writeMetaFile } from '@app/utils';
 
@@ -17,7 +17,7 @@ export const saveFunctions = (
   functions.forEach(({ content, id, type }) => {
     const functionMeta: FunctionMeta = { content, type };
 
-    fs.mkdirSync(path.join(CACHE_FOLDER_PATH, id), { recursive: true });
+    fs.mkdirSync(path.join(CACHE_FOLDER_PATH, id, CALLS_FOLDER_NAME), { recursive: true });
 
     writeMetaFile(path.join(CACHE_FOLDER_PATH, id, META_FILE_NAME), functionMeta);
   });
